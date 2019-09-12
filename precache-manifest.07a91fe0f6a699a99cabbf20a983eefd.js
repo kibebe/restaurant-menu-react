ac58922fa2ac83d62dee4c4bd767129d8e6c3385
Ciira Kibebe <cik.cir@gmail.com>
2019-09-12 09:43:36 +0300

    Updates

---
+++ precache-manifest.07a91fe0f6a699a99cabbf20a983eefd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c3b9319dde718ef617f7083b2bc198",
+    "url": "/restuarant-menu-react/index.html"
+  },
+  {
+    "revision": "9498af5f04b7ea2918bd",
+    "url": "/restuarant-menu-react/static/css/2.c708510d.chunk.css"
+  },
+  {
+    "revision": "452b096db864694afa30",
+    "url": "/restuarant-menu-react/static/css/main.af33ab15.chunk.css"
+  },
+  {
+    "revision": "9498af5f04b7ea2918bd",
+    "url": "/restuarant-menu-react/static/js/2.22065111.chunk.js"
+  },
+  {
+    "revision": "452b096db864694afa30",
+    "url": "/restuarant-menu-react/static/js/main.26e46463.chunk.js"
+  },
+  {
+    "revision": "79f0aac187941aa76250",
+    "url": "/restuarant-menu-react/static/js/runtime~main.b446dc7b.js"
+  }
+]);
